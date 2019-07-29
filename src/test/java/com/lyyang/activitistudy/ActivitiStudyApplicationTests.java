43d00a77d5297f8beb9346d0864427175f4308c3
Yunyang Li <lyyang233@gmail.com>
2019-07-29 19:02:19 +0800

    请假流程模拟

--- src/test/java/com/lyyang/activitistudy/ActivitiStudyApplicationTests.java
+++ src/test/java/com/lyyang/activitistudy/ActivitiStudyApplicationTests.java
@@ -4,6 +4,12 @@ import org.activiti.engine.*;
 import org.activiti.engine.identity.Group;
 import org.activiti.engine.identity.User;
 
+import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.repository.ProcessDefinitionQuery;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.task.TaskQuery;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +19,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 
 import static org.junit.Assert.*;
+
+/**
+ * @author yyli16
+ * @date 2019/7/29
+ * @time 15:05
+ *
+ * 此代码纯面向过程，无设计模式，无可扩展性，只用于activiti基础功能演示
+ */
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -35,12 +49,32 @@ public class ActivitiStudyApplicationTests {
     @Autowired
     private FormService formService;
 
-//    @Autowired
-//    private UserRepository userRepository;
-
     @Test
     public void contextLoads() {
 
+        //创建用户
+        createUser();
+
+        //创建组
+//        createGroup();
+
+        //将用户加入组
+//        addUserToGroup();
+
+        //流程部署
+        myDeploy();
+
+        //启动流程
+        startProcess();
+
+        //用户任务管理
+        userTaskManagement();
+    }
+
+    /**
+     * 创建组
+     */
+    private void createGroup(){
         // 创建并保存组对象
         Group group = identityService.newGroup("deptLeader");
         group.setName("部门领导");
@@ -49,33 +83,130 @@ public class ActivitiStudyApplicationTests {
         // 验证组是否已保存成功，首先需要创建组查询对象
         List<Group> groupList = identityService.createGroupQuery().groupId("deptLeader").list();
         assertEquals(1, groupList.size());
+    }
 
+    /**
+     *  创建用户
+     */
+    private void createUser(){
         // 创建并保存用户对象
-        User user = identityService.newUser("henryyan");
-        user.setFirstName("Henry");
-        user.setLastName("Yan");
-        user.setEmail("yanhonglei@gmail.com");
-        identityService.saveUser(user);
-        // 验证用户是否保存成功
-        User userInDb = identityService.createUserQuery().userId("henryyan").singleResult();
-        assertNotNull(userInDb);
+//        User user = identityService.newUser("henryyan");
+//        user.setFirstName("Henry");
+//        user.setLastName("Yan");
+//        user.setEmail("yanhonglei@gmail.com");
+//        identityService.saveUser(user);
+//        // 验证用户是否保存成功
+//        User userInDb = identityService.createUserQuery().userId("henryyan").singleResult();
+//        assertNotNull(userInDb);
 
+        User user1 = identityService.newUser("小明");
+        identityService.saveUser(user1);
 
-        // 把用户henryyan加入到组deptLeader中
-        identityService.createMembership("henryyan", "deptLeader");
+        User user2 = identityService.newUser("班长");
+        identityService.saveUser(user2);
 
+        User user3 = identityService.newUser("班主任");
+        identityService.saveUser(user3);
 
-        // 删除用户
-        identityService.deleteUser("henryyan");
-        // 验证是否删除成功
-        userInDb = identityService.createUserQuery().userId("henryyan").singleResult();
-        assertNull(userInDb);
+        User user4 = identityService.newUser("教导主任");
+        identityService.saveUser(user4);
 
+    }
+
+    private void queryGroup(){
+        // 查询属于组deptLeader的用户
+        User userInGroup = identityService.createUserQuery().memberOfGroup("deptLeader").singleResult();
+        assertNotNull(userInGroup);
+        assertEquals("henryyan", userInGroup.getId());
+    }
+
+    private void queryUser(){
+        // 查询henryyan所属组
+        Group groupContainsHenryyan = identityService.createGroupQuery().groupMember("henryyan").singleResult();
+        assertNotNull(groupContainsHenryyan);
+        assertEquals("deptLeader", groupContainsHenryyan.getId());
+    }
+
+    /**
+     * 删除组
+     */
+    private void deleteGroup(){
         // 删除组
         identityService.deleteGroup("deptLeader");
         // 验证是否删除成功
-        groupList = identityService.createGroupQuery().groupId("deptLeader").list();
+        List<Group> groupList = identityService.createGroupQuery().groupId("deptLeader").list();
         assertEquals(0, groupList.size());
     }
 
+    /**
+     * 删除用户
+     */
+    private void deleteUser(){
+        // 删除用户
+        identityService.deleteUser("henryyan");
+        // 验证是否删除成功
+        User userInDb = identityService.createUserQuery().userId("henryyan").singleResult();
+        assertNull(userInDb);
+    }
+
+    /**
+     * 将用户加入组
+     */
+    private void addUserToGroup(){
+        // 把用户henryyan加入到组deptLeader中
+        identityService.createMembership("henryyan", "deptLeader");
+    }
+
+    /**
+     * 将用户从组中移除
+     */
+    private void deleteUserFromGroup(){
+        // 把用户henryyan加入到组deptLeader中
+        identityService.deleteMembership("henryyan", "deptLeader");
+    }
+
+    /**
+     * 流程部署
+     */
+    private void myDeploy(){
+        //部署bpmn文件
+        String bpmnClasspath = "bpmn/flow2.bpmn";
+        // 创建部署构建器
+        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
+        // 添加资源
+        deploymentBuilder.addClasspathResource(bpmnClasspath);
+        // 执行部署
+        deploymentBuilder.deploy();
+
+        // 验证流程定义是否部署成功
+        ProcessDefinitionQuery processDefinitionQuery = repositoryService.createProcessDefinitionQuery();
+        List<ProcessDefinition> myProcess = processDefinitionQuery.processDefinitionKey("qjlc").list();
+        System.out.println("process count: " + myProcess.size());
+//        assertEquals(1, count);
+    }
+
+    /**
+     * 启动流程
+     */
+    private void startProcess(){
+        // 启动流程
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("qjlc");
+//        ProcessInstance processInstance = runtimeService.startProcessInstanceById("myProcess_2:3:2505");
+        assertNotNull(processInstance);
+    }
+
+    /**
+     * 用户任务管理
+     */
+    private void userTaskManagement(){
+        String userId = "班长";
+        //henryyan作为候选人的任务
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateUser(userId).list();
+        System.out.println("test");
+//        assertNotNull(task);
+//        //签收任务
+//        taskService.claim(task.getId(), userId);
+//        //处理任务
+//        taskService.complete(task.getId());
+    }
 }
